9e1a1ab681adebd49052a5e434058e0fa9da1bce
GermanoJota <davialmeidabarros.dab.6@gmail.com>
2026-09-21 20:36:38 -0300

    Update main.py

--- algo_prova 3°Bim/main.py
+++ algo_prova 3°Bim/main.py
@@ -2,17 +2,15 @@ import json
 import random
 from datetime import datetime as dt
 
-
 def carregar_conta():
     try:
         with open("conta.json", "r", encoding="utf-8") as f:
             return json.load(f)
     except FileNotFoundError:
         return None
-
 def salvar_conta(conta):
     try:
-        with open("conta.json", "w", encoding="utf-8") as f:
+        with open("conta.json", "a", encoding="utf-8") as f:
             json.dump(conta, f, ensure_ascii=False, indent=2)
         if conta != None:    
             print("conta criada com sucesso")
@@ -38,12 +36,17 @@ def depositar(conta, valor):
     conta["extrato"].append(extrato)
 
 def criar_conta():
-    n_conta = random.randint(10000, 99999)
-    conta = {
-        "nome": input("Qual seu nome? "),
+    nome =input("Qual seu nome? ")
+    try:
+        idade = int(input("Qual sua idade? "))
+    except TypeError:
+        print("erro, isso não é um numero real")
+    
+    conta_nova = {
+        nome:{
         "cpf": input("Qual seu cpf? "),
         "email": input("Qual seu email? "),
-        "idade": input("Me diga sua idade: "),
+        "idade": idade,
         "endereço": {
             "cep": input("Qual seu cep? "),
             "rua": input("Qual sua rua? "),
@@ -51,12 +54,16 @@ def criar_conta():
             },
         "estado_civil": input("Qual seu estado civil? "),
         "agencia": "8181",
-        "conta": n_conta
-        }
-    verificacao_idade = int(conta["idade"])
+        "conta": str(random.randint(10000, 99999))
+        }}
+    
+    verificacao_idade = int(conta[f"{nome}"]["idade"])
     if verificacao_idade < 18:
         print("Você não pode ter uma conta no banco!!")
         conta = None
+        return
+    conta
     return conta
 
-salvar_conta(criar_conta())
+
+
